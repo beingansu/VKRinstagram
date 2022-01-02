e9bb607bf694de51c6c059c819c760f59bebfd88
Vijay Kumar <contactvkr@yahoo.com>
2022-01-02 20:37:55 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$dwlname = "Instagram";
+$webname = "VKR4YouTube";
+$dlapi = "i1nTs2tHa3gEr4aOm5F6F7I8C9I0A1L2V3K4R5"; //API For Video Download
+//$fetchapi = "";    //API For YouTube Search Key
+?>
